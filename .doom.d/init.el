ab224df2ca3eea8edeabef2fa8df6797ab45b9d3
CrownCrafter <46600783+CrownCrafter@users.noreply.github.com>
2022-05-05 06:40:51 +0000

    Debloat DOOM

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -31,12 +31,12 @@
        (emoji +unicode)  ; 🙂
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       hydra
+       ;;hydra
        indent-guides     ; highlighted indent columns
        (ligatures +extra); ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink cursor line after big motions
+       ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup
@@ -49,8 +49,8 @@
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
-      workspaces        ; tab emulation, persistence & separate workspaces
-      zen               ; distraction-free coding or writing
+       workspaces        ; tab emulation, persistence & separate workspaces
+       zen               ; distraction-free coding or writing
 
       :editor
       (evil +everywhere); come to the dark side, we have cookies
@@ -59,10 +59,10 @@
       (format)  ; automated prettiness
       ;;god               ; run Emacs commands without modifier keys
       ;;lispy             ; vim for lisp, for people who don't like vim
-      multiple-cursors  ; editing in many places at once
+      ;;multiple-cursors  ; editing in many places at once
       ;;objed             ; text object editing for the innocent
       ;;parinfer          ; turn lisp into python, sort of
-      rotate-text       ; cycle region at point between text candidates
+      ;;rotate-text       ; cycle region at point between text candidates
       snippets          ; my elves. They type so I don't have to
       word-wrap         ; soft wrapping with language-aware indent
 
@@ -82,7 +82,7 @@
       :checkers
       syntax              ; tasing you for every semicolon you forget
       (:if (executable-find "aspell") spell) ; tasing you for misspelling mispelling
-      grammar           ; tasing grammar mistake every you make
+      ;;grammar           ; tasing grammar mistake every you make
 
       :tools
       ;;ansible
@@ -98,10 +98,10 @@
       lsp
       magit             ; a git porcelain for Emacs
       make              ; run make tasks from Emacs
-      pass              ; password manager for nerds
+      ;;pass              ; password manager for nerds
       pdf               ; pdf enhancements
       ;;prodigy           ; FIXME managing external services & code builders
-      rgb               ; creating color strings
+      ;;rgb               ; creating color strings
       ;;taskrunner        ; taskrunner for all your projects
       ;;terraform         ; infrastructure as code
       ;;tmux              ; an API for interacting with tmux
